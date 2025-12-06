dd431a1cb231f3a0a8c4011be5b5506c43337b33
Luca Schiavoi <jerv.norsk@gmail.com>
2025-12-06 08:09:21 +0100

    #65 Development Workspace
    - (WIP) Test on GitHub with subrepository pathnames

--- services/website/src/app/story/page.tsx
+++ services/website/src/app/story/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import './page.css'
+import {usePathname} from "next/navigation";
 
 export interface StoryPageProps {
   branch: string,
 }
 
 export default function StoryPage(p: StoryPageProps) {
+  const pathname = usePathname();
+
   return (
-    <iframe src={`/blob/story/${p.branch}/index.html`}/>
+    <div className={"w-full h-full"}>
+      <p>{pathname}</p>
+      <iframe src={`/blob/story/${p.branch}/index.html`}/>
+    </div>
   );
 }
