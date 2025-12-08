8df4afe10c82af23115aa709023b5a3a3d7df6a6
Luca Schiavoi <jerv.norsk@gmail.com>
2025-12-08 18:07:24 +0100

    #63 DevLog - Week 07
    - Fix website version management

--- services/website/src/app/game/page.tsx
+++ services/website/src/app/game/page.tsx
@@ -2,6 +2,7 @@
 
 import "./page.css"
 import {addBasePath} from "next/dist/client/add-base-path";
+import {useSearchParams} from "next/navigation";
 
 export interface GamePageProps {
   version: string;
@@ -9,7 +10,10 @@ export interface GamePageProps {
 }
 
 export default function GamePage(p: GamePageProps) {
-  const version = p.version || "next";
+  const searchParams = useSearchParams();
+
+  const version = p.version || searchParams.get("version") || "next";
+  const debug = p.debug || searchParams.has("debug");
 
   const sourceLink = addBasePath(`/blob/game/${version}/index.html`);
   return (
--- services/website/src/app/story/page.tsx
+++ services/website/src/app/story/page.tsx
@@ -3,6 +3,7 @@
 import './page.css'
 
 import {addBasePath} from "next/dist/client/add-base-path";
+import {useSearchParams} from "next/navigation";
 
 export interface StoryPageProps {
   version: string;
@@ -10,8 +11,12 @@ export interface StoryPageProps {
 }
 
 export default function StoryPage(p: StoryPageProps) {
-  const version = p.version || "next";
-  const sourceParams = new URLSearchParams();
+  const searchParams = useSearchParams();
+
+  const version = p.version || searchParams.get("version") || "next";
+  const debug = p.debug || searchParams.has("debug");
+
+  const sourceParams = new URLSearchParams(useSearchParams());
   const sourceLink = addBasePath(`/lib/story/index.html`);
 
   sourceParams.append("story", addBasePath(`/blob/story/${version}/story.json`))
