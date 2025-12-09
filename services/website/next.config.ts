21aea5dfc937a1497378febcc316cf99d8e525dc
Luca Schiavoi <jerv.norsk@gmail.com>
2025-12-09 08:41:02 +0100

    #64 Development Workspace - Service & Automations (WIP)
    - Test next game build on website

--- services/website/next.config.ts
+++ services/website/next.config.ts
@@ -15,18 +15,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/blob/:path',
-        headers: [
-          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
-          { key: 'Pragma', value: 'no-cache' },
-          { key: 'Expires', value: '0' },
-        ],
-      },
-    ]
-  },
-}
