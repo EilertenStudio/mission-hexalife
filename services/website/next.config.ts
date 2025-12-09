db410dab4d31de386dff4463cab7c205b7421539
Luca Schiavoi <jerv.norsk@gmail.com>
2025-12-09 08:32:30 +0100

    #64 Development Workspace - Service & Automations (WIP)
    - Test next game build on website
    - Disable blob caching on website

--- services/website/next.config.ts
+++ services/website/next.config.ts
@@ -15,3 +15,18 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/blob/:path',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ]
+  },
+}
